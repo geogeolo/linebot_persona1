4833a69bfb0d3cfd749dfe8b36fd81058f91bc0f
geogeolo <163122629+geogeolo@users.noreply.github.com>
2024-03-19 15:50:46 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import time
 
 app = Flask(__name__)
-
+count = 0
 openai.api_key = os.getenv('OPENAI_API_KEY')
 line_bot_api = LineBotApi(os.getenv('CHANNEL_ACCESS_TOKEN'))
 handler1 = WebhookHandler(os.getenv('CHANNEL_SECRET'))
@@ -54,7 +54,10 @@ def handle_message(event):
         ret = response['choices'][0]['message']['content'].strip() 
     except:
         ret = '發生錯誤！'
-   
+        
+    global count
+    count+=1
+    return f"<h1> You clicked this page {count} times </h1>"
     line_bot_api.reply_message(event.reply_token,TextSendMessage(text=ret))
 
 if __name__ == '__main__':
